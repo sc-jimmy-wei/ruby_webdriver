ebed085a63b04814fa1ad19eba0e4c5fde1ea387
Jimmy <jimmy@glooko.com>
2016-07-03 21:24:49 -0700

    add baby sign up flow test

--- ruby_webdriver/page_element.rb
+++ ruby_webdriver/page_element.rb
@@ -9,7 +9,7 @@ class HomePage
 	h1(:home_title, :xpath => '//*[@id="page-content-wrapper"]/div/div/div/h1')
 	image(:baby_pic, :alt => 'Happy baby')
 	link(:sign_up_link, :href => 'babies/new')
-
+	link(:show_all_babies_link, :href => 'babies')
 	#define functions used in HOME PAGE
 	
 end
@@ -20,7 +20,36 @@ class SignUpPage
 
 	#define element in SIGN UP PAGE
 	h1(:sign_up_title, :xpath => '/html/body/h1')
+	text_field(:first_name, :id => 'baby_first_name')
+	text_field(:last_name, :id => 'baby_last_name')
+	checkbox(:metric, :id => 'baby_metric')
+	text_field(:height, :id => 'baby_height')
+	text_field(:weight, :id => 'baby_weight')
+	text_field(:temperature, :id => 'baby_temperature')
+	button(:create_button, :value => 'Create Baby')
 
 	#define functions used in SIGN UP PAGE
 
+end
+
+class BabyListPage
+	include PageObject
+	page_url("http://localhost:3000/babies")
+
+	#define element in BABY LIST PAGE
+	h1(:baby_list_title, :xpath => '/html/body/h1')
+	table(:baby_list_table, :xpath => '/html/body/table')
+	link(:find_steve, :xpath => '//*[contains(text(), "Steve Yang")]')
+
+	#define functoins used in BABY LIST PAGE
+	# def locate_row_by_text(text)
+	# 	match_row = baby_list_table_element.find{ |row| row.text.include? text}
+	# 	fail "Could not locate the row with value #{text}" if match_row.nil?
+	# 	match_row
+	# end
+
+	# def click_match_row(text)
+	# 	match_row = locate_row_by_text(text)
+	# 	match_row.link_element.click
+	# end
 end
--- ruby_webdriver/rspec_test.rb
+++ ruby_webdriver/rspec_test.rb
@@ -38,4 +38,26 @@ describe "Fitness Tracker" do
 		@wait.until {sign_up_page.sign_up_title}
 		expect(sign_up_page.sign_up_title == "Sign up").to be true
 	end
+
+	it "check sign up flow for new baby" do
+		home_page = HomePage.new(@driver)
+		home_page.goto
+		@wait.until {home_page.sign_up_link_element}
+		home_page.sign_up_link
+		sign_up_page = SignUpPage.new(@driver)
+		@wait.until {sign_up_page.first_name_element}
+		sign_up_page.first_name = "Steve"
+		sign_up_page.last_name = "Yang"
+		sign_up_page.check_metric
+		sign_up_page.height = 75
+		sign_up_page.weight = 6
+		sign_up_page.temperature = 27
+		sign_up_page.create_button
+		@wait.until {home_page.show_all_babies_link_element}
+		home_page.show_all_babies_link
+		baby_list_page = BabyListPage.new(@driver)
+		@wait.until {baby_list_page.baby_list_table_element}
+		expect(baby_list_page.find_steve_element.visible?).to be true
+	end
+
 end
