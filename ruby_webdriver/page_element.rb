81e743414db5fd338ad2c5d9967729a321a85d61
Jimmy <jimmy@glooko.com>
2016-07-06 11:23:35 -0700

    add test for sign up flow with random string and number

--- ruby_webdriver/page_element.rb
+++ ruby_webdriver/page_element.rb
@@ -4,11 +4,13 @@ require "page-object"
 #https://github.com/cheezy/page-object/wiki/Elements
 #gem page object element
 
+#http://www.rubydoc.info/github/cheezy/page-object/PageObject
+
 class HomePage
 	include PageObject
 	page_url("http://localhost:3000")
 
-	#define element in HOME PAGE
+	#define elements in HOME PAGE
 	h1(:home_title, :xpath => '//*[@id="page-content-wrapper"]/div/div/div/h1')
 	image(:baby_pic, :alt => 'Happy baby')
 	link(:sign_up_link, :href => 'babies/new')
@@ -21,9 +23,9 @@ class SignUpPage
 	include PageObject
 	page_url("http://localhost:3000/babies/new")
 
-	#define element in SIGN UP PAGE
+	#define elements in SIGN UP PAGE
 	h1(:sign_up_title, :xpath => '/html/body/h1')
-	text_field(:first_name, :id => 'baby_first_name')
+	text_field(:first_name, id: 'baby_first_name')
 	text_field(:last_name, :id => 'baby_last_name')
 	checkbox(:metric, :id => 'baby_metric')
 	text_field(:height, :id => 'baby_height')
@@ -32,6 +34,15 @@ class SignUpPage
 	button(:create_button, :value => 'Create Baby')
 
 	#define functions used in SIGN UP PAGE
+	def generate_random_string()
+		randomString = [*('A'..'z')].sample(8).join
+		randomString
+	end
+
+	def generate_random_number()
+		random_number = [*('0'..'9')].sample(2).join
+		random_number
+	end
 
 end
 
@@ -39,20 +50,11 @@ class BabyListPage
 	include PageObject
 	page_url("http://localhost:3000/babies")
 
-	#define element in BABY LIST PAGE
+	#define elements in BABY LIST PAGE
 	h1(:baby_list_title, :xpath => '/html/body/h1')
 	table(:baby_list_table, :xpath => '/html/body/table')
 	link(:find_steve, :xpath => '//*[contains(text(), "Steve Yang")]')
 
 	#define functoins used in BABY LIST PAGE
-	# def locate_row_by_text(text)
-	# 	match_row = baby_list_table_element.find{ |row| row.text.include? text}
-	# 	fail "Could not locate the row with value #{text}" if match_row.nil?
-	# 	match_row
-	# end
 
-	# def click_match_row(text)
-	# 	match_row = locate_row_by_text(text)
-	# 	match_row.link_element.click
-	# end
 end
--- ruby_webdriver/rspec_test.rb
+++ ruby_webdriver/rspec_test.rb
@@ -15,21 +15,21 @@ describe "Fitness Tracker" do
 		@driver.quit
 	end
 
-	it "check if Home Page can load" do
+	it "load home page" do
 		home_page = HomePage.new(@driver)
 		home_page.goto
 		@wait.until {home_page.home_title}
 		expect(home_page.home_title == "Welcome to Fitness Tracker").to be true
 	end
 
-	it "check if baby picture is displayed in home page" do
+	it "load home page picture" do
 		home_page = HomePage.new(@driver)
 		home_page.goto
 		@wait.until {home_page.baby_pic_element}
 		expect(home_page.baby_pic_element.visible?).to be true
 	end
 
-	it "check if Sign Up Page can load" do
+	it "load sign up page" do
 		home_page = HomePage.new(@driver)
 		home_page.goto
 		@wait.until {home_page.sign_up_link_element}
@@ -39,13 +39,12 @@ describe "Fitness Tracker" do
 		expect(sign_up_page.sign_up_title == "Sign up").to be true
 	end
 
-	it "check sign up flow for new baby" do
+	it "sign up new baby" do
 		home_page = HomePage.new(@driver)
 		home_page.goto
-		@wait.until {home_page.sign_up_link_element}
-		home_page.sign_up_link
+		home_page.sign_up_link_element.when_visible.click
 		sign_up_page = SignUpPage.new(@driver)
-		@wait.until {sign_up_page.first_name_element}
+		@wait.until {sign_up_page.first_name?}
 		sign_up_page.first_name = "Steve"
 		sign_up_page.last_name = "Yang"
 		sign_up_page.check_metric
@@ -58,6 +57,29 @@ describe "Fitness Tracker" do
 		baby_list_page = BabyListPage.new(@driver)
 		@wait.until {baby_list_page.baby_list_table_element}
 		expect(baby_list_page.find_steve_element.visible?).to be true
+	end
+
+	it "sign up flow" do
+		home_page = HomePage.new(@driver)
+		home_page.goto
+		home_page.sign_up_link_element.when_visible.click
+		sign_up_page = SignUpPage.new(@driver)
+		@wait.until {sign_up_page.first_name?}
+		sign_up_page.first_name = sign_up_page.generate_random_string
+		puts sign_up_page.first_name
+		find_xpath = "//*[contains(text(), \'%s\')]" % [sign_up_page.first_name]
+		@wait.until {sign_up_page.last_name?}
+		sign_up_page.last_name = sign_up_page.generate_random_string
+		sign_up_page.check_metric
+		sign_up_page.height = sign_up_page.generate_random_number
+		sign_up_page.weight = sign_up_page.generate_random_number
+		sign_up_page.temperature = sign_up_page.generate_random_number
+		sign_up_page.create_button
+		home_page.show_all_babies_link_element.when_visible.click
+		baby_list_page = BabyListPage.new(@driver)
+		@wait.until {baby_list_page.baby_list_table?}
+		test_baby = @driver.find_element(:xpath, find_xpath)
+		expect(test_baby.displayed?).to be true
 	end
 
 end
